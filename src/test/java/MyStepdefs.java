9b98ecc807a5cd0a48a2cc4f3c5386adc1e78ba1
johan <johan@thylin.io>
2026-04-09 10:08:02 +0200

    Re-structuring

--- src/test/java/MyStepdefs.java
+++ src/test/java/MyStepdefs.java
@@ -1,5 +1,4 @@
 import io.cucumber.java.After;
-import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -10,6 +9,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -17,108 +17,15 @@ import java.time.Duration;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 
-public class MyStepdefs {
 
-    private WebDriver driver;
-
-    @Before
-    public void chromeDriver() {
-        ChromeOptions options = new ChromeOptions();
-        //options.addArguments("--headless=new");
-        driver = new ChromeDriver(options);
-    }
-    @Before
-    public void firefoxDriver() {
-        driver = new FirefoxDriver();
-    }
-    @After
-    public void tearDown() {
-        driver.quit();
-    }
+public class MyStepdefs extends HelperMethods {
 
     @When("User has entered valid inputs")
     public void user_has_entered_valid_inputs() {
         fillRegistrationForm(firstName(), lastName(), email(), email(), password(), password());
 
     }
-
-    private void clicker(By by) {
-        WebElement element = new WebDriverWait(this.driver, Duration.ofSeconds(10))
-                .until(ExpectedConditions.elementToBeClickable(by));
-        element.click();
-    }
-
-    private void picker(By by, String text) {
-        WebElement element = new WebDriverWait(this.driver, Duration.ofSeconds(10))
-                .until(ExpectedConditions.elementToBeClickable(by));
-        element.clear();
-        element.sendKeys(text);
-    }
-
-    private void submiter(By by) {
-        WebElement element = new WebDriverWait(this.driver, Duration.ofSeconds(10))
-                .until(ExpectedConditions.elementToBeClickable(by));
-        element.submit();
-    }
-
-
-    protected static String firstName() {        return "John";}
-
-    protected static String lastName() {      return "John";}
-
-    protected static String password() {
-        return "Johnny";
-    }
-
-    protected static String passwordShort() {
-        return "John";
-    }
-
-    protected static String passwordLong() {
-        return "JohnnyIsAMenace2Society";
-    }
-
-    protected static String email() {
-        return "john@john.se";
-    }
-
-    protected static String misMatchEmail() {
-        return "johnny@john.se";
-    }
-
-    protected static String invalidEmail() {
-        return "johnse";
-    }
-
-    protected static String emptyField() {
-        return "";
-    }
-
-    protected static String validNumber() {
-        return "12/12/1990";
-    }
-
-    protected static String invalidChars() {
-        return "@@!!Räksmörgås";
-    }
-
-    protected static String underEighteen() {
-        return "12/12/2022";
-    }
-
-    protected static void tabAndType(WebDriver driver, String text) {
-        driver.switchTo().activeElement().sendKeys(Keys.TAB);
-        driver.switchTo().activeElement().sendKeys(text);
-    }
-
-    protected static void tab(WebDriver driver) {
-        driver.switchTo().activeElement().sendKeys(Keys.TAB);
-    }
-
-
-
 
     @Then("I get a membersnumber")
     public void iGetAMembersnumber() {
@@ -142,7 +49,7 @@ public class MyStepdefs {
     }
 
     @When("Accept all terms")
-    public void acceptAllTerms() throws InterruptedException {
+    public void acceptAllTerms() {
         // Account confirmation
         clicker(By.cssSelector("label[for='sign_up_25']"));
         clicker(By.cssSelector("label[for='sign_up_26']"));
@@ -154,74 +61,6 @@ public class MyStepdefs {
     public void focusIsOnTheFirstField() {
         picker(By.cssSelector("input#dp[name='DateOfBirth'][type='datetime']"),"");
     }
-
-    @Then("User does not reach the radiobuttons by tabbing")
-    public void userDoesNotReachTheRadiobuttonsByTabbing() {
-        boolean reachedRadio = false;
-
-        WebElement current = driver.switchTo().activeElement();
-
-
-        for (int i = 0; i < 20; i++) {
-
-            current.sendKeys(Keys.TAB);
-
-
-            current = driver.switchTo().activeElement();
-
-
-            String tag = current.getTagName();
-            String type = current.getAttribute("type");
-
-
-            if ("input".equalsIgnoreCase(tag) && "radio".equalsIgnoreCase(type)) {
-                reachedRadio = true;
-                break;
-            }
-        }
-
-        assertFalse(reachedRadio);
-    }
-
-    private void fillRegistrationForm(String firstname,
-                                      String lastname,
-                                      String emailValue,
-                                      String comfirmEmail,
-                                      String pwd,
-                                      String confirm) {
-
-       driver.findElement(By.cssSelector("[id='member_firstname'][name='Forename'][type='text']")).sendKeys(firstname);
-
-        driver.findElement(By.cssSelector("[id='member_lastname'][name='Surname'][type='text']")).sendKeys(lastname);
-
-        driver.findElement(By.cssSelector("[id='member_emailaddress'][name='EmailAddress'][type='text']")).sendKeys(emailValue);
-
-        driver.findElement(By.cssSelector("[id='member_confirmemailaddress'][name='ConfirmEmailAddress'][type='text']")).sendKeys(comfirmEmail);
-
-        driver.findElement(By.cssSelector("[id='signupunlicenced_password'][name='Password'][type='password']")).sendKeys(pwd);
-
-        driver.findElement(By.cssSelector("[id='signupunlicenced_confirmpassword'][name='ConfirmPassword'][type='password']")).sendKeys(confirm);
-    }
-
-    private void fillRegistrationFormParental(String underEighteen,
-                                              String firstname,
-                                              String lastname,
-                                              String emailValue,
-                                              String comfirmEmail) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        driver.findElement(By.name("DateOfBirth")).clear();
-        driver.findElement(By.name("DateOfBirth")).sendKeys(underEighteen);
-
-        WebElement parentName = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[id='sign_up_6'][name='ParentForename'][type='text']")));
-        parentName.sendKeys(firstname);
-
-        driver.findElement(By.cssSelector("[id='sign_up_8'][name='ParentSurname'][type='text']")).sendKeys(lastname);
-
-        driver.findElement(By.cssSelector("[id='signup_supporteraccount_parentemail'][name='ParentEmailAddress'][type='text']")).sendKeys(emailValue);
-
-        driver.findElement(By.cssSelector("[id='signup_supporteraccount_parentemailconfirm'][name='ParentConfirmEmailAddress'][type='text']")).sendKeys(comfirmEmail);
-    }
-
 
     @When("User has entered invalid inputs {string}")
     public void userHasEnteredInvalidInputs(String caseName) {
@@ -256,17 +95,10 @@ public class MyStepdefs {
         }
     }
 
-
     @When("User click on join button")
     public void userClickOnJoinButton()  {
                 submiter(By.cssSelector("[class='btn btn-big red']"));
     }
-
-    @Given("User is on registrationpage")
-    public void userIsOnRegistrationpage() {
-        driver.get("file:///Users/johan/Downloads/Register/registrering.html");
-    }
-
 
     @Then("User should get an {string}")
     public void userShouldGetAn(String errorMessage) {
@@ -301,15 +133,33 @@ public class MyStepdefs {
 
     }
 
-    @Given("The uses uses {string}")
-    public void theUsesUses(String browser) {
+    @Given("The user uses {string}")
+    public void theUserUses(String browser) {
         if (browser.equalsIgnoreCase("chrome")) {
-            driver = new ChromeDriver();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless=new");
+            driver = new ChromeDriver(options);
         } else {
-            driver = new FirefoxDriver();
+            FirefoxOptions options = new FirefoxOptions();
+            options.addArguments("--headless");
+            driver = new FirefoxDriver(options);
         }
         driver.get("file:///Users/johan/Downloads/Register/registrering.html");
     }
+
+    @When("Accepts all terms by using keyboard only")
+    public void acceptsAllTermsByUsingKeyboardOnly() {
+        tab(driver);
+        tab(driver);
+        tab(driver);
+
+    }
+
+    @When("Navigates by tabbing and joins")
+    public void navigatesByTabbingAndJoins() {
+        driver.switchTo().activeElement().sendKeys(Keys.TAB);
+        driver.switchTo().activeElement().sendKeys(Keys.ENTER);
+            }
 }
 
 
--- src/test/resources/features/Register.feature
+++ src/test/resources/features/Register.feature
@@ -1,53 +1,72 @@
 Feature: Register on Basketballpage
 
-  Background:
-    Given User is on registrationpage
-
-
-
-
-
   Scenario Outline: Successfull Registration
-
+    Given The user uses "<browser>"
     And focus is on the first field date of birth
     And User has entered valid inputs
     When Accept all terms
     And User click on join button
     Then I get a membersnumber
     Examples:
+    |browser|
+    |chrome |
+    |firefox|
 
 
   Scenario Outline: Unsuccessfull registration
-
+    Given The user uses "<browser>"
     And focus is on the first field date of birth
     When User has entered invalid inputs "<caseName>"
     When Accept all terms
     And User click on join button
     Then User should get an "<errorMessage>"
   Examples:
-      |caseName | errorMessage |
-     | invalidEmail | The Email Address field is not a valid e-mail address |
-      |misMatchEmail | Confirm Email Address does not match                 |
-      | missingPwd |  Password is required                                    |
-      | misMatchPwd |  Password did not match                                 |
-      |pwdShort |   Password must be between 5 and 20 characters              |
-      |pwdLong |   Password must be between 5 and 20 characters               |
-      |missingFirstName |First Name is required                             |
-      |missingLastName |Last Name is required                              |
+     |browser |caseName | errorMessage |
+     |chrome |invalidEmail | The Email Address field is not a valid e-mail address |
+     |chrome |misMatchEmail | Confirm Email Address does not match                 |
+     |chrome | missingPwd |  Password is required                                    |
+     |chrome| misMatchPwd |  Password did not match                                 |
+     |chrome  |pwdShort |   Password must be between 5 and 20 characters              |
+     |chrome |pwdLong |   Password must be between 5 and 20 characters               |
+     |chrome|missingFirstName |First Name is required                             |
+     |chrome|missingLastName |Last Name is required                              |
+     |firefox |invalidEmail | The Email Address field is not a valid e-mail address |
+     |firefox |misMatchEmail | Confirm Email Address does not match                 |
+     |firefox | missingPwd |  Password is required                                    |
+     |firefox| misMatchPwd |  Password did not match                                 |
+     |firefox  |pwdShort |   Password must be between 5 and 20 characters              |
+     |firefox |pwdLong |   Password must be between 5 and 20 characters               |
+     |firefox|missingFirstName |First Name is required                             |
+     |firefox|missingLastName |Last Name is required                              |
 
 
   Scenario Outline: Not accepting all terms
+    Given The user uses "<browser>"
     And focus is on the first field date of birth
     And User has entered valid inputs
     When Not accepting all "<terms>"
     And User click on join button
     Then User should get an "<errorMessage>"
   Examples:
-      |terms  | errorMessage |
-      |understood       |You must confirm that you have read and accepted our Terms and Conditions      |
-      |overEighteen       |You must confirm that you are over 18 or a person with parental responsibility      |
-      |codeOfConduct       |You must confirm that you have read, understood and agree to the Code of Ethics and Conduct      |
+      |browser|terms  | errorMessage |
+      |chrome|understood       |You must confirm that you have read and accepted our Terms and Conditions      |
+      |chrome|overEighteen       |You must confirm that you are over 18 or a person with parental responsibility      |
+      |chrome|codeOfConduct       |You must confirm that you have read, understood and agree to the Code of Ethics and Conduct      |
+      |firefox|understood       |You must confirm that you have read and accepted our Terms and Conditions      |
+      |firefox|overEighteen       |You must confirm that you are over 18 or a person with parental responsibility      |
+      |firefox|codeOfConduct       |You must confirm that you have read, understood and agree to the Code of Ethics and Conduct      |
 
-    Scenario: Check WCAG protocol
+    Scenario Outline: Check WCAG protocol
+      Given The user uses "<browser>"
+      And focus is on the first field date of birth
+      When User has entered valid inputs by tabbing
+      And Accepts all terms by using keyboard only
+      And Navigates by tabbing and joins
+      Then I get a membersnumber
+
+      Examples:
+        | browser|
+        | chrome|
+        | firefox|
 
 
